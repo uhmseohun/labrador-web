bc22f14d08bee97b385498bb36421054e8cfae7c
uhmtoto <uhmtotow@gmail.com>
2019-07-22 15:41:13 +0900

    feat: place page

--- src/views/Place.vue
+++ src/views/Place.vue
@@ -1,14 +1,40 @@
 <script>
+import PlaceItem from '@/components/PlaceItem.vue'
+
 export default {
-  name: 'Place'
+  name: 'Place',
+  components: { PlaceItem },
+  data () {
+    return {
+      places: []
+    }
+  },
+  created () {
+    this.$api.get('/place')
+      .then(r => { this.places = r.data })
+      .catch(e => this.$swal('에러!', e.response.data.message, 'error'))
+  }
 }
 </script>
 
 <template>
 <div class="place">
   <h1 class="place__title">장소</h1>
+  <place-item
+    :key="`place-${i}`"
+    v-for="(place, i) in places"
+    class="place__item"
+    :place="place"
+  />
 </div>
 </template>
 
 <style lang="scss" scoped>
+.place {
+  &__item {
+    &:not(:first-child) {
+      margin-top: 10px;
+    }
+  }
+}
 </style>
