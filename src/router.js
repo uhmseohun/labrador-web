1379cab03ec3aa553070fbec714510a13003fe2e
uhmtoto <uhmtotow@gmail.com>
2019-07-24 11:28:17 +0900

    chore(router): add meta, auth control for each route

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import axios from 'axios'
 
 import Login from '@/views/Login.vue'
+
 import Place from '@/views/Place.vue'
 import Notification from '@/views/Notification.vue'
 import Setting from '@/views/Setting.vue'
@@ -24,7 +25,12 @@ const requireAuth = (to, from, next) => {
   next('/auth')
 }
 
-export default new Router({
+const forbidAuth = (to, from, next) => {
+  if (localStorage.accessToken) return next('/')
+  next()
+}
+
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -39,57 +45,89 @@ export default new Router({
     {
       path: '/auth',
       name: 'login',
-      component: Login
+      meta: {
+        title: '로그인'
+      },
+      component: Login,
+      beforeEnter: forbidAuth
+    },
+    {
+      path: '/auth/join',
+      name: 'join',
+      component: () => import(/* webpackChunkName: "join" */ '@/views/Join.vue'),
+      beforeEnter: forbidAuth
     },
     {
       path: '/place',
       name: 'place',
+      meta: {
+        title: '장소 목록'
+      },
       component: Place,
       beforeEnter: requireAuth
     },
     {
       path: '/add/place',
       name: 'addPlace',
+      meta: {
+        title: '장소 추가'
+      },
       component: AddPlace,
       beforeEnter: requireAuth
     },
     {
       path: '/notification',
       name: 'notifiaction',
+      meta: {
+        title: '알림'
+      },
       component: Notification,
       beforeEnter: requireAuth
     },
     {
       path: '/timeline',
       name: 'timeline',
+      meta: {
+        title: '타임라인'
+      },
       component: Timeline,
       beforeEnter: requireAuth
     },
     {
       path: '/device',
       name: 'device',
+      meta: {
+        title: '기기 목록'
+      },
       component: Device,
       beforeEnter: requireAuth
     },
     {
       path: '/add/device',
       name: 'addDevice',
+      meta: {
+        title: '기기 추가'
+      },
       component: AddDevice,
       beforeEnter: requireAuth
     },
     {
       path: '/setting',
       name: 'setting',
+      meta: {
+        title: '설정'
+      },
       component: Setting,
       beforeEnter: requireAuth
     }
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    // }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = `LABRADOR - ${to.meta.title}`
+  }
+  next()
+})
+
+export default router
